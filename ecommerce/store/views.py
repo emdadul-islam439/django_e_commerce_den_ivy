8d501f2ce279185cbe404be68c436e83c660a1b1
Emdadul_Islam <emdadulislam439@gmail.com>
2022-08-30 17:52:47 +0600

    successfully shown all-info into the 'CART' page

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -34,6 +34,12 @@ def cart(request):
         items = order.orderitem_set.all()
         noOfCartItems = order.get_number_of_items
     else:
+        try:
+            cart = json.loads(request.COOKIES['cart'])
+        except:
+            cart = {}
+            
+        print('Cart: ', cart)
         items = []
         order = {
             'get_cart_total': 0,
@@ -41,6 +47,33 @@ def cart(request):
             'shipping': False
         }
         noOfCartItems = order['get_number_of_items']
+        
+        for id in cart:
+            try:
+                noOfCartItems += cart[id]['quantity']
+
+                product = Product.objects.get(id= id)
+                total = (product.price * cart[id]['quantity'])
+
+                order['get_cart_total'] += total
+                order['get_number_of_items'] += cart[id]['quantity']
+
+                item = {
+                    'product': {
+                        'id': product.id,
+                        'name': product.name,
+                        'price': product.price,
+                        'imageURL': product.imageURL
+                    },
+                    'quantity': cart[id]['quantity'],
+                    'get_total': total
+                }
+                items.append(item)
+
+                if product.digital == False:
+                    order['shipping'] = True 
+            except:
+                pass
     
     context={ 'items': items, 'order': order, 'noOfCartItems':  noOfCartItems }
     return render(request, 'store/cart.html', context)
