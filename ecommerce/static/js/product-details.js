772be2ed03511b2913dda018195516fc7278f5fa
Emdadul_Islam <emdadulislam439@gmail.com>
2022-09-07 18:57:29 +0600

    finished designing 'product-details' page

--- ecommerce/static/js/product-details.js
+++ ecommerce/static/js/product-details.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
+$(document).ready(function () {
     //-- Click on detail
-    $("ul.menu-items > li").on("click",function(){
-        $("ul.menu-items > li").removeClass("active");
+    $("ul.details-page-menu-items > li").on("click", function () {
+        $("ul.details-page-menu-items > li").removeClass("active");
         $(this).addClass("active");
     })
 
-    $(".attr,.attr2").on("click",function(){
+    $(".attr,.attr2").on("click", function () {
         var clase = $(this).attr("class");
 
         $("." + clase).removeClass("active");
@@ -13,21 +13,21 @@ $(document).ready(function(){
     })
 
     //-- Click on QUANTITY
-    $(".btn-minus").on("click",function(){
+    $(".btn-minus").on("click", function () {
         var now = $(".section > div > input").val();
-        if ($.isNumeric(now)){
-            if (parseInt(now) -1 > 0){ now--;}
+        if ($.isNumeric(now)) {
+            if (parseInt(now) - 1 > 0) { now--; }
             $(".section > div > input").val(now);
-        }else{
+        } else {
             $(".section > div > input").val("1");
         }
-    })            
-    $(".btn-plus").on("click",function(){
+    })
+    $(".btn-plus").on("click", function () {
         var now = $(".section > div > input").val();
-        if ($.isNumeric(now)){
-            $(".section > div > input").val(parseInt(now)+1);
-        }else{
+        if ($.isNumeric(now)) {
+            $(".section > div > input").val(parseInt(now) + 1);
+        } else {
             $(".section > div > input").val("1");
         }
-    })                        
+    })
 }) 
