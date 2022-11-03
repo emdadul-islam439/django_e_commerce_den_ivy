2e3512162be966a66110a6e3dceebe9dea4b1afb
Emdadul_Islam <emdadulislam439@gmail.com>
2022-11-03 21:05:51 +0600

    finished all admin/cart-details page related all operations-> changed template, update quantity, remove item, add item

--- ecommerce/admin/views.py
+++ ecommerce/admin/views.py
@@ -1,6 +1,6 @@
 import json
 from django.http import JsonResponse
-from store.models import Order, OrderItem, Product
+from store.models import Order, OrderItem, Product, Cart, CartItem
 from django.views.generic import DetailView
 from store.utils import cartData, getTrackInfoList
 
@@ -119,6 +119,107 @@ def addAdminOrderItems(request, **kwargs):
                 orderItem.quantity = 1
                 orderItem.save()
         response_message = f'{len(productIdList)} orderItem(s) ADDED successfully'
+    except:
+        response_message = 'Failed! Error occured while adding item!'
+        
+    return JsonResponse(response_message, safe=False)
+
+
+
+
+
+class AdminCartDetailView(DetailView):
+    login_required = True
+    template_name: str = "admin/store/cart/admin_cart_details.html"
+    context_object_name: str = "cart"
+    model = Cart
+    
+    def get_context_data(self,*args, **kwargs):
+        context = super(AdminCartDetailView, self).get_context_data(*args,**kwargs)
+        context['items'] = self.items
+        context['products'] = self.products
+        context['noOfCartItems'] = self.noOfCartItems
+        return context
+    
+    
+    def get(self, request, *args, **kwargs):
+        self.cookieData = cartData(request = request)
+        self.noOfCartItems = self.cookieData['noOfCartItems']
+        self.cart_id = self.kwargs.get('pk')
+        self.items = CartItem.objects.filter(cart__id = self.cart_id)
+        self.products = Product.objects.all()
+        
+        print(f'.............................cart_id = {self.cart_id}  items = {self.items},  noOfCartItems = {self.noOfCartItems}')
+        return super(AdminCartDetailView, self).get(request, *args, **kwargs) 
+    
+    
+    
+# @csrf_exempt
+def updateAdminCartItem(request, **kwargs):
+    data = json.loads(request.body)
+    
+    itemId = data['itemId']
+    action = data['action']
+    
+    print('in UPDATE-ADMIN-ORDER-ITEM().............')
+    print(f"itemId = {itemId} ")
+    print(f"action = {action}")
+    
+    cartItem = CartItem.objects.get(id = itemId)
+    
+    if action == 'add':
+        cartItem.quantity += 1
+        response_message = 'cartItem was INCREASED successfully'
+    elif action == 'remove':
+        if cartItem.quantity > 1:
+            cartItem.quantity -= 1
+            response_message = 'cartItem was DECREASED successfully'
+        else:
+            response_message = 'Failed! Only one item left!'
+    elif action == 'check-uncheck':
+        cartItem.is_checked = not cartItem.is_checked
+        response_message = 'Item was CHECKED/UNCHECKED successfully'
+    
+    cartItem.save()
+    return JsonResponse(response_message, safe=False)
+
+
+# @csrf_exempt
+def removeAdminCartItem(request, **kwargs):
+    data = json.loads(request.body)
+    
+    itemId = data['itemId']
+    
+    cart_id = kwargs.get('pk')
+    items = CartItem.objects.filter(cart__id = cart_id)
+    itemCount = len(items)
+    
+    print('in REMOVE-ADMIN-ORDER-ITEM().............')
+    print(f"itemId = {itemId} cart_id = {cart_id}  itemCount = {itemCount}")
+    
+    cartItem = CartItem.objects.get(id = itemId)
+    cartItem.delete()
+    response_message = 'cartItem was REMOVED successfully'
+        
+    return JsonResponse(response_message, safe=False)
+
+
+# @csrf_exempt
+def addAdminCartItems(request, **kwargs):
+    data = json.loads(request.body)
+    
+    productIdList = data['productIdList']
+    cart_id = kwargs.get('pk')
+    cart = Cart.objects.get(id=cart_id)
+    print(f"cart_id = {cart_id}  productIdList = {productIdList}")
+    try:
+        for productId in productIdList:
+            product = Product.objects.get(id = productId)
+            cartItem, created = CartItem.objects.get_or_create(cart = cart, product = product)
+            if cartItem.quantity == 0:
+                cartItem.quantity = 1
+                cartItem.save()
+        response_message = f'{len(productIdList)} cartItem(s) ADDED successfully'
     except:
         response_message = 'Failed! Error occured while adding item!'
         
