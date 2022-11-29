6ded484272cd87897d9ad74a70dbcc96de3b1b15
Emdadul_Islam <emdadulislam439@gmail.com>
2022-11-29 08:07:13 +0600

    started 'background_tasks' app for celery and other background related jobs

--- ecommerce/store/admin.py
+++ ecommerce/store/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from store.models import Customer, Order, OrderItem, Product, Cart, CartItem, ShippingAddress, WishListItem, PurchasedItem, SoldItem, Stock
+from store.models import Order, OrderItem, Product, Cart, CartItem, ShippingAddress, WishListItem, PurchasedItem, SoldItem, Stock
+from store.models import Customer
 
 # Register your models here.
 class CustomerAdminModel(admin.ModelAdmin):
